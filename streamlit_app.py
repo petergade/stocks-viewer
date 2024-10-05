4d3cf190ec8593686813c201e91841336e01e6fc
Karel Bem <karel.bem@gmail.com>
2024-10-05 23:18:55 +0200

    SMA10 + 100

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@ import streamlit as st
 from polygon import RESTClient
 import pandas as pd
 from polygon.rest.models import Timeframe
+from datetime import datetime, timedelta
+
+date_from = datetime.today() - timedelta(days = (365 * 5) - 7)
+date_to = datetime.today()
 
 st.header('Polygon + Streamlit Demo App')
 symbol = st.text_input('Enter a stock symbol', 'AAPL')
@@ -54,8 +58,21 @@ if rows[0][2].button('Get historical'):
 
 if rows[1][0].button('Get SMA'):
     try:
+        sma_10 = client.get_sma(
+            ticker = symbol,
+            timestamp_gte = date_from,
+            timespan = 'day',
+            window = 10,
+            series_type = 'close',
+            limit = 5000
+        )
+        chart_data_sma10 = pd.DataFrame(sma_10.values)
+        chart_data_sma10['date'] = chart_data_sma10['timestamp'].apply(lambda x: pd.to_datetime(x * 1000000))
+        chart_data_sma10.rename(columns = {'value': 'SMA_10'}, inplace = True)
+
         sma_50 = client.get_sma(
             ticker = symbol,
+            timestamp_gte = date_from,
             timespan = 'day',
             window = 50,
             series_type = 'close',
@@ -65,8 +82,21 @@ if rows[1][0].button('Get SMA'):
         chart_data_sma50['date'] = chart_data_sma50['timestamp'].apply(lambda x: pd.to_datetime(x*1000000))
         chart_data_sma50.rename(columns = {'value': 'SMA_50'}, inplace = True)
 
+        sma_100 = client.get_sma(
+            ticker = symbol,
+            timestamp_gte = date_from,
+            timespan = 'day',
+            window = 100,
+            series_type = 'close',
+            limit = 5000
+        )
+        chart_data_sma100 = pd.DataFrame(sma_100.values)
+        chart_data_sma100['date'] = chart_data_sma100['timestamp'].apply(lambda x: pd.to_datetime(x * 1000000))
+        chart_data_sma100.rename(columns = {'value': 'SMA_100'}, inplace = True)
+
         sma_200 = client.get_sma(
             ticker = symbol,
+            timestamp_gte = date_from,
             timespan = 'day',
             window = 200,
             series_type = 'close',
@@ -80,21 +110,23 @@ if rows[1][0].button('Get SMA'):
             ticker = symbol,
             multiplier = 1,
             timespan = 'day',
-            from_ = '2020-01-01',
-            to = '2024-10-05'
+            from_ = date_from,
+            to = date_to
         )
         chart_data_prices = pd.DataFrame(prices)
         chart_data_prices['date'] = chart_data_prices['timestamp'].apply(lambda x: pd.to_datetime(x * 1000000))
         chart_data_prices.rename(columns = {'close': 'Price'}, inplace = True)
 
-        chart_data = pd.merge(chart_data_sma50[['date', 'SMA_50']], chart_data_sma200[['date', 'SMA_200']], on = 'date',
-                              how = 'inner')
 
+
+        chart_data = pd.merge(chart_data_sma50[['date', 'SMA_50']], chart_data_sma200[['date', 'SMA_200']], on = 'date', how = 'inner')
+        chart_data = pd.merge(chart_data, chart_data_sma10[['date', 'SMA_10']], on = 'date', how = 'inner')
+        chart_data = pd.merge(chart_data, chart_data_sma100[['date', 'SMA_100']], on = 'date', how = 'inner')
         chart_data = pd.merge(chart_data, chart_data_prices[['date', 'Price']], on = 'date', how = 'inner')
 
         chart_data
 
-        st.line_chart(chart_data.set_index('date'), y = ['SMA_50', 'SMA_200', 'Price'], x_label = 'Date', y_label = 'Price')
+        st.line_chart(chart_data.set_index('date'), y = ['SMA_10', 'SMA_50', 'Price'], x_label = 'Date', y_label = 'Price')
     except Exception as e:
         st.exception(f'Exception: {e}')
 
